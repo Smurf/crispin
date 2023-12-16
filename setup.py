06eaf83648c8bd56b7f4c748ae75cf11690bd369
Sam Coles <sammuel.coles@gmail.com>
2023-12-16 02:09:02 -0600

    Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='crispin',
+    version='0.1.0',
+    author='Samuel Coles',
+    author_email='me@smurf.codes',
+    description='A utility to generate Kickstarts and ISOs for RHEL-alike Linux distros.',
+    packages=['crispin'],
+    entry_points = {
+        'console_scripts': ['crispin = crispin.crispin:main']
+    },
+    install_requires = ['Jinja2==3.1.2','MarkupSafe==2.1.3']
+)
